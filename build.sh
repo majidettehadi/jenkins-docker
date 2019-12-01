d3645e98348e713af09734a1c1943cf1f9c3ebd9
Seyedmajid Etehadi <ettehadi.m@gmail.com>
2019-12-01 17:41:32 +0330

    Update to 2.190.3

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@
 
 set -x
 . build.properties
-docker build --force-rm  -t $REPO_NAME:latest .
+docker build --force-rm \
+    --build-arg JENKINS_VERSION=$JENKINS_VERSION \
+    -t $REPO_NAME:latest \
+    -t $REPO_NAME:$JENKINS_VERSION .
